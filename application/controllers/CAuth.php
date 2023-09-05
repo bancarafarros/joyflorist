579cd1199ce260fb958c0fec25a0f876cfdce00b
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-09-05 18:12:40 +0700

    pembatasan hak akses di halaman login

--- application/controllers/CAuth.php
+++ application/controllers/CAuth.php
@@ -1,6 +1,17 @@
 <?php
 
 class CAuth extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+
+        if ($this->session->userdata('role_id') == '1') {
+            redirect(base_url('admin/CDashboard'));
+        
+        } else if ($this->session->userdata('role_id') == '2') {
+            redirect(base_url('CDashboard'));
+        }
+    }
+    
     public function index() {
         $this->load->view('VLogin');
     }
